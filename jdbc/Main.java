18d5dca7cccbe46b1713c5caeaab475eea0c7e79
kaming325 <110148041+kaming325@users.noreply.github.com>
2023-04-03 00:32:56 +0800

    Update Main.java

--- jdbc/Main.java
+++ jdbc/Main.java
@@ -5,10 +5,10 @@ import java.sql.Statement;
 
 public class Main{
     static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";  
-    static final String DB_URL = "jdbc:mysql://localhost:3306/robotDogService";
+    static final String DB_URL = "jdbc:mysql://localhost:3306/********";
     
     static final String USER = "root";
-    static final String PASS = "Ming@12321";
+    static final String PASS = "********";
     public static void main(String[] args) {
         Connection connection = null;
         Statement statement = null;
